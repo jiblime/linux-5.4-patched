cea9b6a42432565a359020c855cea1659dfadaf3
jiblime <47689567+jiblime@users.noreply.github.com>
2019-11-27 11:24:56 -0800

    Patched 5.4 with: Master branch (26/11) of Clear Linux's patchset, except for the ones that cause incompat or already upstream. This requires the HJL binutils patch.
    0007-v5.3-fsync.patch
    Add-git-version-of-zenpower-as-a-builtin-module.patch
    amd-iommu-fix.patch
    enable_additional_cpu_optimizations_for_gcc_v9.1+_kernel_v4.13+.patch
    gentoo-flavor-5.3.patch
    iwd-rfkill.patch
    Replaced-drivers-hwmon-it87.c.patch
    experimental-x86-65-opts.patch (replaced arch/x86/Makefile's -falign-loops=32:225:16 and changed i386 to k6)
    
    This is a pretty messy tree but I am currently running it so it..works

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -52,6 +52,9 @@ unsigned int pci_pm_d3_delay;
 
 static void pci_pme_list_scan(struct work_struct *work);
 
+static void pci_dev_save_and_disable(struct pci_dev *dev);
+static void pci_dev_restore(struct pci_dev *dev);
+
 static LIST_HEAD(pci_pme_list);
 static DEFINE_MUTEX(pci_pme_list_mutex);
 static DECLARE_DELAYED_WORK(pci_pme_work, pci_pme_list_scan);
@@ -61,7 +64,7 @@ struct pci_pme_device {
 	struct pci_dev *dev;
 };
 
-#define PME_TIMEOUT 1000 /* How long between PME checks */
+#define PME_TIMEOUT 4000 /* How long between PME checks */
 
 static void pci_dev_d3_sleep(struct pci_dev *dev)
 {
@@ -1407,15 +1410,7 @@ static void pci_restore_config_space(struct pci_dev *pdev)
 		pci_restore_config_space_range(pdev, 4, 9, 10, false);
 		pci_restore_config_space_range(pdev, 0, 3, 0, false);
 	} else if (pdev->hdr_type == PCI_HEADER_TYPE_BRIDGE) {
-		pci_restore_config_space_range(pdev, 12, 15, 0, false);
-
-		/*
-		 * Force rewriting of prefetch registers to avoid S3 resume
-		 * issues on Intel PCI bridges that occur when these
-		 * registers are not explicitly written.
-		 */
-		pci_restore_config_space_range(pdev, 9, 11, 0, true);
-		pci_restore_config_space_range(pdev, 0, 8, 0, false);
+		pci_restore_config_space_range(pdev, 0, 15, 0, true);
 	} else {
 		pci_restore_config_space_range(pdev, 0, 15, 0, false);
 	}
@@ -4660,6 +4655,8 @@ void pci_reset_secondary_bus(struct pci_dev *dev)
 {
 	u16 ctrl;
 
+	pci_dev_save_and_disable(dev);
+
 	pci_read_config_word(dev, PCI_BRIDGE_CONTROL, &ctrl);
 	ctrl |= PCI_BRIDGE_CTL_BUS_RESET;
 	pci_write_config_word(dev, PCI_BRIDGE_CONTROL, ctrl);
@@ -4672,6 +4669,8 @@ void pci_reset_secondary_bus(struct pci_dev *dev)
 
 	ctrl &= ~PCI_BRIDGE_CTL_BUS_RESET;
 	pci_write_config_word(dev, PCI_BRIDGE_CONTROL, ctrl);
+
+	pci_dev_restore(dev);
 
 	/*
 	 * Trhfa for conventional PCI is 2^25 clock cycles.
--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -225,8 +225,6 @@ exit_idle:
 static void do_idle(void)
 {
 	int cpu = smp_processor_id();
-	bool pending = false;
-
 	/*
 	 * If the arch has a polling bit, we maintain an invariant:
 	 *
@@ -237,10 +235,7 @@ static void do_idle(void)
 	 */
 
 	__current_set_polling();
-	if (unlikely(softirq_pending(cpu)))
-		pending = true;
-	else
-		tick_nohz_idle_enter();
+	tick_nohz_idle_enter();
 
 	while (!need_resched()) {
 		rmb();
@@ -278,8 +273,7 @@ static void do_idle(void)
 	 * an IPI to fold the state for us.
 	 */
 	preempt_set_need_resched();
-	if (!pending)
-		tick_nohz_idle_exit();
+	tick_nohz_idle_exit();
 	__current_clr_polling();
 
 	/*
@@ -361,7 +355,6 @@ void cpu_startup_entry(enum cpuhp_state state)
 		do_idle();
 }
 
-#ifndef CONFIG_SCHED_MUQSS
 /*
  * idle-task scheduling class.
  */
@@ -486,4 +479,3 @@ const struct sched_class idle_sched_class = {
 	.switched_to		= switched_to_idle,
 	.update_curr		= update_curr_idle,
 };
-#endif /* CONFIG_SCHED_MUQSS */
